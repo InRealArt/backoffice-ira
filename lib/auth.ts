cab0bb8bcd1df93717d7f16695dc3d08d67d83eb
gilbruno <gilles_bruno@gmx.fr>
2025-10-31 16:52:45 +0100

    add new columns on users

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,7 +8,19 @@ export const auth = betterAuth({
     }),
     // on cible explicitement les modèles Prisma mappés au schéma backoffice
     user: {
-        modelName: 'BackofficeAuthUser'
+        modelName: 'BackofficeAuthUser',
+        additionalFields: {
+            role: {
+                type: 'string',
+                required: false,
+                input: false
+            },
+            artistId: {
+                type: 'number',
+                required: false,
+                input: true
+            }
+        }
     },
     session: {
         modelName: 'BackofficeAuthSession'
